55b6341e1038cd3d27cc4aec988147d7fae8246a
eric <ericbushnell@gmail.com>
2022-07-17 19:05:23 -0400

    Begin createGrid()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Test");
+const gridContainer = document.querySelector("#grid-container");
+const gridSize = 600;
+let gridItem; 
+let cellCount = 16;
+
+createGrid();
+
+function createGrid() {
+  let cellSize = cellCount => gridSize / cellCount;
+
+  for (i = 0; i < cellCount; i++) {
+    gridItem = document.createElement("div");
+    gridItem.setAttribute("style", `background-color: yellow; width: ${cellSize(cellCount)}px; height: ${cellSize(cellCount)}px;`)
+    gridContainer.appendChild(gridItem);
+  }
+}
