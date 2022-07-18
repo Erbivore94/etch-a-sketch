ea5569d560d935e99c6e165549a7b9d44b439bdd
eric <ericbushnell@gmail.com>
2022-07-18 15:33:49 -0400

    Rename divs and css attributes for clarity
    gridContainerBorder is now gridContainer.
    gridContainer is now grid.
    gridItems are now cells.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const gridContainer = document.querySelector("#grid-container");
+const grid = document.querySelector("#grid");
 const gridSize = 600;
-let gridItem; 
+let cells; 
 let cellCount = 16;
 
 createGrid();
@@ -10,9 +10,9 @@ function createGrid() {
 
   for (i = 0; i < cellCount; i++) {
     for (j = 0; j < cellCount; j++) {
-      gridItem = document.createElement("div");
-      gridItem.setAttribute("style", `background-color: yellow; width: ${cellSize(cellCount)}px; height: ${cellSize(cellCount)}px;`)
-      gridContainer.appendChild(gridItem);
+      cells = document.createElement("div");
+      cells.setAttribute("style", `background-color: yellow; width: ${cellSize(cellCount)}px; height: ${cellSize(cellCount)}px;`)
+      grid.appendChild(cells);
     }
   }
 }
